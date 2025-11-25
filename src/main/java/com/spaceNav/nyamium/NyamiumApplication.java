d0057bad95c5bdff1ef8690b4e083019ff840675
iKniMiohC <fnwm7401@naver.com>
2025-11-25 15:01:55 +0900

    #59 [FIX] created at 추가 및 probability 값이 1 넘을 시 1로 수정

--- src/main/java/com/spaceNav/nyamium/NyamiumApplication.java
+++ src/main/java/com/spaceNav/nyamium/NyamiumApplication.java
@@ -2,8 +2,10 @@ package com.spaceNav.nyamium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class NyamiumApplication {
 
 	public static void main(String[] args) {
